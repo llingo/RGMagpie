cb8fccec17ca044d9bb80a533413767bce9384d6
Lingo <llingo@kakao.com>
2022-09-13 19:54:01 +0900

    feat: RGMagpieError 에러 타입 구현

---
+++ RGMagpie/Classes/RGMagpieError.swift
@@ -0,0 +1,22 @@
+//
+//  RGMagpieError.swift
+//  RGMagpie
+//
+//  Created by Ringo on 2022/09/13.
+//
+
+import Foundation
+
+public enum RGMagpieError: Error {
+  case unknownError
+  case badRequestError
+  case unAuthorizedError
+  case notFoundError
+  case internalServerError
+  case serviceUnavailableError
+  case invalidateResponseError
+  case errorIsOccurred(_ error: String)
+
+  case invalidateURLError
+  case invalidateImageError
+}

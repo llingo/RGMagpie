521869beeaed56ee9c0813739d3695e66572d4f3
Lingo <llingo@kakao.com>
2022-09-13 19:53:02 +0900

    feat: RGMagpie 타입 추가

--- RGMagpie/Classes/RGMagpie.swift
+++ RGMagpie/Classes/RGMagpie.swift
@@ -1,1 +1,30 @@
+//
+//  RGMagpie.swift
+//  RGMagpie
+//
+//  Created by Ringo on 2022/09/13.
+//  Copyright © 2022 RGMagpie. All rights reserved.
+//
+
+import UIKit
+
+public struct RGMagpie<Base> {
+  public let base: Base
+
+  public init(_ base: Base) {
+    self.base = base
+  }
+}
+
+public protocol RGMagpieCompatible: AnyObject {}
+
+extension RGMagpieCompatible {
+  public var mp: RGMagpie<Self> {
+    get { RGMagpie(self) }
+  }
+}
+
+// MARK: - Extension
+
+extension UIImageView: RGMagpieCompatible {}
 

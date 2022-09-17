12d49be4f7cab5dc3b8057b1054192d11220f7a8
Ringo <llingo@kakao.com>
2022-09-17 10:16:17 +0900

    feat: Etag 기능 추가 #3

--- RGMagpie/Classes/Extension/UIImageView+RGMagpie.swift
+++ RGMagpie/Classes/Extension/UIImageView+RGMagpie.swift
@@ -29,17 +29,76 @@ extension RGMagpie where Base: UIImageView {
       task.cancel()
     }
 
+    let task = loadImage(with: urlString, placeholder: placeholder, completion: completion)
+    mutatingSelf.imageTask = task
+
+    return task
+  }
+
+  private func loadImage(
+    with urlString: String,
+    placeholder: UIImage?,
+    completion: ((Result<UIImage, RGMagpieError>) -> Void)?
+  ) -> ImageDownloadTask? {
     guard let url = URL(string: urlString) else {
       completion?(.failure(.invalidateURLError))
       return nil
     }
 
+    /*
+     1. ETag 확인
+     - 1-1. 304 Not-Modified라면 Cache 로직 시작
+     - 1-2. 200 OK로 새로운 ETag을 받으면 Memory, Disk 캐시에 저장
+
+     setImage(with:placeholder:completion:)
+        |---304 Not-Modified---> Check memory / disk cache -> API Request
+        |
+        |---200 OK ETag: abc---> Save to memory / disk cache
+    */
+
+    let etag = UserDefaults.standard.string(forKey: url.path)
+
+    return ImageDownloader.default.downloadImage(with: url, etag: etag) { result in
+      switch result {
+      case .success(let item):
+        if let image = UIImage(data: item.imageData) {
+          UserDefaults.standard.set(item.etag, forKey: url.path)
+          ImageCache.default.store(item.imageData, forKey: url)
+
+          DispatchQueue.main.async { [weak view = base as UIImageView] in
+            view?.image = image
+            completion?(.success(image))
+          }
+          return
+        }
+        completion?(.failure(.invalidateImageError))
+
+      case .failure(let error):
+        if case .notModifiedError = error {
+          if let cachedImageData = ImageCache.default.retrieve(forKey: url) {
+            DispatchQueue.main.async { [weak view = base as UIImageView] in
+              view?.image = UIImage(data: cachedImageData)
+            }
+          }
+          completion?(.failure(error))
+          return
+        }
+        completion?(.failure(error))
+      }
+
+      DispatchQueue.main.async { [weak view = base as UIImageView] in
+        view?.image = placeholder
+      }
+    }
+
+    // MARK: - Legacy
+    /*
     if let cachedImageData = ImageCache.default.retrieve(forKey: url) {
       base.image = UIImage(data: cachedImageData)
       return nil
     }
 
-    let task = ImageDownloader.default.downloadImage(with: url) { result in
+    return ImageDownloader.default.downloadImage(with: url) { result in
       switch result {
       case .success(let item):
         if let image = UIImage(data: item.imageData) {
@@ -61,8 +120,6 @@ extension RGMagpie where Base: UIImageView {
         view?.image = placeholder
       }
     }
-
-    mutatingSelf.imageTask = task
-    return task
+   */
   }
 }
